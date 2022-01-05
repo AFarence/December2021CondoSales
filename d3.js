a28dcce563a1ca2e917730f71fd64d4e56365284
XCTKD <aefarence@gmail.com>
2022-01-04 20:24:51 -0500

    Update d3.js

--- d3.js
+++ d3.js
@@ -1,7 +1,7 @@
 (function () {
 
   // Margin convention
-  const margin = { top: 30, right: 50, bottom: 50, left: 50 }
+  const margin = { top: 10, right: 50, bottom: 50, left: 50 }
   const width = 600 - margin.left - margin.right
   const height = 425 - margin.top - margin.bottom
 
